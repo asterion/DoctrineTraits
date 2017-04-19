385e40330f9e54565ba29ed5a38d6aa05d0cef84
Marcos Matamala <asterion.alfa@gmail.com>
2017-04-18 21:40:36 -0300

    Trait Timestampable

---
+++ src/asterion/Doctrine/Traits/Timestampable.php
@@ -0,0 +1,65 @@
+<?php
+namespace Asterion\Doctrine\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait Timestampable
+{
+    /**
+     * @var datetimetz $createdAt
+     *
+     * @ORM\Column(name="created_at", type="datetimetz")
+     */
+    private $createdAt;
+
+    /**
+     * @var datetimetz $updatedAt
+     *
+     * @ORM\Column(name="updated_at", type="datetimetz")
+     */
+    private $updatedAt;
+
+    /**
+     * Get createdAt
+     *
+     * @return datetimetz
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param datetimetz $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return datetimetz
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param datetimetz $updatedAt
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
